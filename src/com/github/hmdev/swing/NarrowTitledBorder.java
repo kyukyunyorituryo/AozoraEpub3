828ea4b9450dc016bd992a1f285f56563a36ade9
急急如律令 <kyukyunyorituryo@live.jp>
2024-09-14 02:10:44 +0900

    @Serial

--- src/com/github/hmdev/swing/NarrowTitledBorder.java
+++ src/com/github/hmdev/swing/NarrowTitledBorder.java
@@ -2,11 +2,13 @@ package com.github.hmdev.swing;
 
 import java.awt.Component;
 import java.awt.Insets;
+import java.io.Serial;
 
 import javax.swing.border.TitledBorder;
 
 public class NarrowTitledBorder extends TitledBorder
 {
+	@Serial
 	private static final long serialVersionUID = 1L;
 	
 	public NarrowTitledBorder(String title)
